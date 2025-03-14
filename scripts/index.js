74a8623ac0ab24d4a43ebbfde3e50c1fb0110f19
Egor Petin <ep3tin@yandex.ru>
2025-03-15 02:02:24 +0400

    первая попытка

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,32 @@
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
+function addCards(card, deleteCallback) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
 
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardImage.src = card.link;
+  cardImage.alt = card.name;
+  cardTitle.textContent = card.name;
+
+  deleteButton.addEventListener("click", () => {
+    deleteCallback(cardElement);
+  });
+
+  return cardElement;
+}
 // @todo: Функция удаления карточки
-
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
 // @todo: Вывести карточки на страницу
+const placesList = document.querySelector(".places__list");
+
+initialCards.forEach((card) => {
+  const cardElement = addCards(card, deleteCard);
+  placesList.appendChild(cardElement);
+});
